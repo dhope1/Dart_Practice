32e2899de3883b20229119056877eed4e01b4415
dhope1 <davidhopekavuma@gmail.com>
2024-09-19 13:59:32 +0300

    variables done

---
+++ 1.variables.dart
@@ -0,0 +1,23 @@
+void main() {
+  // Declaring an integer variable
+  int age = 25;
+
+  // Declaring a double variable
+  double height = 5.9;
+
+  // Declaring a string variable
+  String name = 'Solomon';
+
+  // Declaring a boolean variable
+  bool isStudent = true;
+
+  // Output the variables
+  print('Name: $name');
+  print('Age: $age');
+  print('Height: $height');
+  print('Is Student: $isStudent');
+
+  // Updating the value of a variable
+  age = 26;
+  print('Updated Age: $age');
+}
